06a05cf9ee821c527cc51c4a836b962a0a7fb00e
Mawuli87 <yawomessie@gmail.com>
2025-01-07 02:16:57 +1300

    Accordion menu

---
+++ src/components/ui/PriceRangeSlider.jsx
@@ -0,0 +1,17 @@
+import Slider from "rc-slider";
+
+const PriceRangeSlider = ({ value, handleChange, minValue, maxValue }) => {
+  return (
+    <>
+      <Slider
+        range
+        minValue={minValue}
+        maxValue={maxValue}
+        value={value}
+        onChange={handleChange}
+      />
+    </>
+  );
+};
+
+export default PriceRangeSlider;
